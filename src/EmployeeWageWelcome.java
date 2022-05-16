cff04f778ca44f7b773936e18941aca069110d79
ankitap2211 <ankitapatil3166@gmail.com>
2022-05-16 23:11:45 +0530

    ADDED:Check employee attendence

--- src/EmployeeWageWelcome.java
+++ src/EmployeeWageWelcome.java
@@ -1,9 +1,9 @@
 public class EmployeeWageWelcome {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program...");
-        final int IS_FULL_TIME=1;
+        final int IS_FULL_TIME = 1;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck==IS_FULL_TIME)
+        if (empCheck == IS_FULL_TIME)
             System.out.println("Employee is Absent");
         else
             System.out.println("Employee is Present");
