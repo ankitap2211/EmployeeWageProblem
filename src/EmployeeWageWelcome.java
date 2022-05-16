24889c033295b8a819f4561c2385017a7d0d9c52
ankitap2211 <ankitapatil3166@gmail.com>
2022-05-16 23:21:22 +0530

    ADDED: Calculate part time employee wage

--- src/EmployeeWageWelcome.java
+++ src/EmployeeWageWelcome.java
@@ -2,14 +2,19 @@ public class EmployeeWageWelcome
 {
     public static void main(String args[])
     {
-        final int IS_FULL_TIME = 1;
+        final int IS_PART_TIME = 1;
+        final int IS_FULL_TIME = 2;
         final int WAGE_PER_HR = 20;
-        int empType = (int) (Math.random() * 100) % 2;
+        int empType = (int) (Math.random() * 100) % 3;
         int workingHours = 0;
         if (empType == IS_FULL_TIME)
         {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present Full time");
             workingHours = 8;
+        } else if (empType == IS_PART_TIME)
+        {
+            System.out.println("Employee is Present Part time");
+            workingHours = 4;
         } else
         {
             System.out.println("Employee is Absent");
